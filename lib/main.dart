d509ee30b697cf4e5209cddfbdb7a78e08c69c87
Abdo Tareq <abdotarekfathy@gmail.com>
2020-05-24 17:24:30 +0200

    feat Add sound player & refactor to use widget functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,9 +9,51 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: SoundButton(),
         ),
       ),
     );
+  }
+}
+
+class SoundButton extends StatefulWidget {
+  @override
+  _SoundButtonState createState() => _SoundButtonState();
+}
+
+class _SoundButtonState extends State<SoundButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        // this for take screen wide (match parent)
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildKey(soundNum: 1, color: Colors.red),
+          buildKey(soundNum: 2, color: Colors.orange),
+          buildKey(soundNum: 3, color: Colors.yellow, text: 'click'),
+          buildKey(soundNum: 4, color: Colors.lightGreen),
+          buildKey(soundNum: 5, color: Colors.green),
+          buildKey(soundNum: 6, color: Colors.blue),
+          buildKey(soundNum: 7, color: Colors.purple, text: 'click me'),
+        ]);
+  }
+
+  Expanded buildKey({int soundNum, MaterialColor color, String text = ''}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            playSound(soundNum);
+          });
+        },
+        color: color,
+        child: Text(text),
+      ),
+    );
+  }
+
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
   }
 }
